c2dca7e57ebe7c438acb22541b997bf954f28646
deepaliupadhyay <upadhyay.deepali@gmail.com>
2018-09-21 13:16:55 -0700

    app checkin

---
+++ realestate/helpers.py
@@ -0,0 +1,12 @@
+from urlparse import parse_qs
+
+
+def parse_query_params(query_string):
+    """
+        Function to parse the query parameter string.
+        """
+    # Parse the query param string
+    query_params = dict(parse_qs(query_string))
+    # Get the value from the list
+    query_params = {k: v[0] for k, v in query_params.items()}
+    return query_params
