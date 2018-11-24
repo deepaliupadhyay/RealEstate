20f81dec210f7c1c89142a7c78139d34527435ce
Rency Joseph <rency@Rencys-MacBook-Air.local>
2018-11-24 15:38:46 -0800

    skip sets for all urls

--- realestate/app.py
+++ realestate/app.py
@@ -7,7 +7,7 @@ import json
 from pprint import pprint
 from flask import Response
 import os
-from predict_price_knn_model import PredictPriceKNNModel
+# from predict_price_knn_model import PredictPriceKNNModel
 from flask import send_file
 
 from ImagesHelper import ImageHelper
@@ -16,7 +16,6 @@ import io
 app = Flask(__name__)
 
 img_helper = ImageHelper()
-
 
 @app.route('/')
 def home():
@@ -37,7 +36,7 @@ def getOneArticle():
     sortv = request.args.get('sortv')
     sort_by= request.args.get('sort_by')
     skip_rec = request.args.get('skip')
-    print "skip_rec ", skip_rec
+
     client = pymongo.MongoClient("mongodb://du1982:forgot@realestate-shard-00-01-pazv8.mongodb.net",
                                  ssl_cert_reqs=ssl.CERT_REQUIRED,
                                  ssl_ca_certs=certifi.where())
@@ -99,6 +98,8 @@ def housedata():
     price_op = request.args.get('price_op')
     sqft = request.args.get('sqft')
     sqft_op = request.args.get('sqft_op')
+    skip_rec = request.args.get('skip')
+
     print baths_op
     print baths
     print beds_op
@@ -114,7 +115,7 @@ def housedata():
                                  ssl_ca_certs=certifi.where())
     mydb = client.realestate
     mycol = mydb["realEstateData"]
-#    mycol.create_index([('BOUNDARY', pymongo.GEOSPHERE)], name='BOUNDARY', default_language='english')
+    mycol.create_index([('BOUNDARY', pymongo.GEOSPHERE)], name='BOUNDARY', default_language='english')
     query = {"BOUNDARY": {
         "$nearSphere": {"$geometry": {"type": "Point", "coordinates": [float(longitude), float(latitude)]},
                         "$maxDistance": 5}}}
@@ -140,14 +141,16 @@ def housedata():
         print "selected operator {}".format('$' + "operator")
         query.update({'SQUARE FEET': {operator: sqft}})
 
-    property = mycol.find(query, {"_id": 0 }).limit(15)
+    no_of_rec_to_skip = int(skip_rec)
+
+    property = mycol.find(query, {"_id": 0 }).skip(no_of_rec_to_skip).limit(15)
     pprint(property)
 
     result = []
     for document in property:
        result.append(document)
 
-    img_helper.get_images_for_property(result)
+    # img_helper.get_images_for_property(result)
 
     pprint(result)
 
@@ -173,22 +176,7 @@ def getData():
     sqft_op = request.args.get('sqft_op')
     skip_rec = request.args.get('skip')
 
-    #rooms
-    print str
-    print baths_op
-    print baths
-    print beds_op
-    print beds
-    print price_op
-    print price
-    print sqft_op
-    print sqft
-
-
-    #if 'str' in request.args:
-    # return 'Hello ' + request.args['str']
-    #else:
-    #    return 'Hello John Doe'
+    no_of_rec_to_skip = int(skip_rec)
     client = pymongo.MongoClient("mongodb://du1982:forgot@realestate-shard-00-01-pazv8.mongodb.net",
                                  ssl_cert_reqs=ssl.CERT_REQUIRED,
                                  ssl_ca_certs=certifi.where())
@@ -222,18 +210,17 @@ def getData():
 
     if x == False:
         query.update({'CITY': {'$regex': str, '$options': 'i'}})
-        outputdata = [data1 for data1 in mycol.find(query, {"_id": 0}).limit(15)]
+        outputdata = [data1 for data1 in mycol.find(query, {"_id": 0}).skip(no_of_rec_to_skip).limit(15)]
 
     if x==True or len(outputdata) == 0:
         query['ADDRESS'] = {'$regex': str, '$options': 'i'}
-        outputdata = [data1 for data1 in mycol.find(query, {"_id": 0}).limit(15)]
+        outputdata = [data1 for data1 in mycol.find(query, {"_id": 0}).skip(no_of_rec_to_skip).limit(15)]
     #if(filter=='baths'):
-
 
     print "Selected query {}".format(query)
 
     #if outputdata is None :
-    #    finaldata = [data1 for data1 in mycol.find({'ADDRESS': {'$regex': '.*str.*'}}, {"_id": 0}).limit(5)]
+    #    finaldata = [data1 for data1 in mycol.find({'ADDRESS': {'$ regex': '.*str.*'}}, {"_id": 0}).limit(5)]
 
     img_helper.get_images_for_property(outputdata)
     return Response(json.dumps(outputdata),  mimetype='application/json')
@@ -245,21 +232,21 @@ def getImage():
     print ("Image path -- {0}".format(image_path))
     return send_file(image_path, mimetype='image/jpg')
 
-
-@app.route('/price_prediction')
-def getPricePrediction():
-    zip_code = request.args.get('zip_code')
-    beds = request.args.get('beds')
-    baths= request.args.get('baths')
-    square_feet = request.args.get('square_feet')
-    lot_size = request.args.get('lot_size')
-    year_build = request.args.get('year_build')
-
-    model = PredictPriceKNNModel()
-    predicted_price = model.customized_train_model(zip_code=zip_code, beds=beds, baths=baths, square_feet=square_feet,
-                                                   lot_size=lot_size,year_build=year_build)
-    print "The predicted price for parameterized property is" + str(predicted_price)
-    return Response(str(predicted_price), mimetype='text')
+#
+# @app.route('/price_prediction')
+# def getPricePrediction():
+#     zip_code = request.args.get('zip_code')
+#     beds = request.args.get('beds')
+#     baths= request.args.get('baths')
+#     square_feet = request.args.get('square_feet')
+#     lot_size = request.args.get('lot_size')
+#     year_build = request.args.get('year_build')
+#
+#     model = PredictPriceKNNModel()
+#     predicted_price = model.customized_train_model(zip_code=zip_code, beds=beds, baths=baths, square_feet=square_feet,
+#                                                    lot_size=lot_size,year_build=year_build)
+#     print "The predicted price for parameterized property is" + str(predicted_price)
+#     return Response(str(predicted_price), mimetype='text')
 
 @app.route('/property_image')
 def get_property_image():
